727c73368b83384ba935cef6fd2d4153d0d79a4e
Graham McIntire <gmcintire@gmail.com>
2011-09-12 13:59:15 -0500

    tests working now

--- test/test_hamradio.rb
+++ test/test_hamradio.rb
@@ -5,5 +5,22 @@ class TestHamradio < Test::Unit::TestCase
 #    flunk "hey buddy, you should probably rename this file and start testing for real"
 #  end
 
+  should "encode gridsquare culleoka" do
+    h = HamRadio.new
+    grid = h.grid_encode([33.113675,-96.487856])
+    assert_equal('EM13sc', grid)
+  end
+
+  should "encode gridsquare altoga" do
+    h = HamRadio.new
+    grid = h.grid_encode([33.24941379,-96.50782389])
+    assert_equal('EM13rf', grid)
+  end
+
+  should "encode gridsquare verona" do
+    h = HamRadio.new
+    grid = h.grid_encode([33.24617412,-96.42647853])
+    assert_equal('EM13sf', grid)
+  end
 
 end
